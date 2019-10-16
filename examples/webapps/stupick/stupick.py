57ebeec338d2ea790e5393517779f339d301fc86
Ethan McGee <bulletshot60@users.noreply.github.com>
2019-10-16 11:32:25 -0400

    Update stupick.py

--- examples/webapps/stupick/stupick.py
+++ examples/webapps/stupick/stupick.py
@@ -1,47 +1,37 @@
+from flask import Flask, send_from_directory
 import os.path
-import bottle
 import random
+import glob
 
 # To create a desktop shortcut using Chrome:
 # "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --chrome-frame --app="http://sschaub.pythonanywhere.com/stupick"
 
-SOUNDS = ["236982__devengarber__jacob-allen-evil-laugh",
-    "347547__masgame__applause",
-    "371339__johanneskristjansson__cheer-crowd",
-    "stupick3-men-cheering"]
+app = Flask(__name__)
+SOUNDS = [os.path.basename(x) for x in glob.glob("sounds/*")]
 
 CUR_DIR = os.path.dirname(__file__)
 students = []
-    
-@bottle.route('/stupick')
-def picker():
 
-    with open(CUR_DIR + '/stupick.html') as htmlfile:
+@app.route('/stupick')
+def picker():
+    with open(os.path.join(CUR_DIR, 'stupick.html')) as htmlfile:
         HTML = htmlfile.read()
 
-    if len(students) == 0:
-        readStudentFile()
-    stuInx = random.randrange(len(students))
-    stuName, stuUsername = students[stuInx]
-    del students[stuInx]
-    effect = SOUNDS[random.randrange(len(SOUNDS))]
+    student = random.choice(students)
+    effect = random.choice(SOUNDS)
 
-    return HTML.format(name=stuName, username=stuUsername, sound=effect)
+    return HTML.format(name=student[0], username=student[1], sound=effect)
 
-def readStudentFile():
-    with open(CUR_DIR + '/cps110.txt') as stufile: 
+@app.route('/<path:filename>')
+def send_static(filename):
+    """Serve up images and sounds."""
+    return send_from_directory(CUR_DIR, filename)
+
+if __name__ == '__main__':
+    with open(os.path.join(CUR_DIR, 'cps110.txt')) as stufile: 
         for student in stufile:
             [_, _, firstname, lastname, username] = student.split(',')        
             students.append((firstname[0] + ". " + lastname, username))
-
     print("Loaded data: ", students)
-
-if __name__ == '__main__':
-
-    @bottle.route('/<filename:path>')
-    def send_static(filename):
-        """Serve up images and sounds."""
-        return bottle.static_file(filename, root='.')
-
-    # Launch the BottlePy dev server
-    bottle.run(host='localhost', debug=True)
+    # Launch the Flask dev server
+    app.run(host='localhost', debug=True)
