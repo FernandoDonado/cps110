15c4b339c7f7d15e85943b393839698909abf978
Stephen Schaub <sschaub@gmail.com>
2019-11-11 11:07:57 -0500

    Update to f-strings

--- examples/webapps/svgdemo/svgdemo.py
+++ examples/webapps/svgdemo/svgdemo.py
@@ -5,16 +5,16 @@ HEIGHT=700
 
 def makedrawing():  
     svgdata = []
-    svgdata.append("<rect x='0' y='0' width='{}' height='{}' fill='black' />".format(WIDTH, HEIGHT))
+    svgdata.append(f"<rect x='0' y='0' width='{WIDTH}' height='{HEIGHT}' fill='black' />")
 
     for x in range(0, WIDTH+1, 100):
-        svgdata.append("<line x1='{0}' y1='0' x2='{0}' y2='{1}' stroke='green' />".format(x, HEIGHT))
+        svgdata.append(f"<line x1='{x}' y1='0' x2='{x}' y2='{HEIGHT}' stroke='green' />")
 
     for y in range(0, HEIGHT+1, 100):
-        svgdata.append("<line x1='0' y1='{0}' x2='{1}' y2='{0}' stroke='green' />".format(y, WIDTH))
+        svgdata.append(f"<line x1='0' y1='{y}' x2='{WIDTH}' y2='{y}' stroke='green' />")
 
-    svgdata.append("<image x='{0}' y='{1}' xlink:href='images/{2}' width='50' height='50' />".format(20, 30, 'duck.png'))
-    svgdata.append("<text x='{0}' y='{1}' font-family='Verdana' font-size='10' stroke='white'>{2}</text>".format(10, 20, 'Some text here'))
+    svgdata.append("<image x='20' y='30' xlink:href='images/duck.png' width='50' height='50' />")
+    svgdata.append("<text x='10' y='20' font-family='Verdana' font-size='10' stroke='white'>Some text here</text>")
         
     svgstring = '\n'.join(svgdata)
     return HTML_PAGE.format(WIDTH, HEIGHT, svgstring)
@@ -34,6 +34,6 @@ HTML_PAGE = """
 """
 
 drawing = makedrawing()
-f = open('demo.html', 'w')
-f.write(drawing)
-f.close()
+with open('demo.html', 'w') as f:
+    f.write(drawing)
+    
